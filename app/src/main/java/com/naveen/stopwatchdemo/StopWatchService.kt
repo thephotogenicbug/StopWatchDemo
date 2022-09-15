bd7633ce43af29ee7882ea6a838de0026fd14934
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-15 18:58:06 +0530

    Create TimerClass, companion object, StopWatchTimerTask inner class

--- app/src/main/java/com/naveen/stopwatchdemo/StopWatchService.kt
+++ app/src/main/java/com/naveen/stopwatchdemo/StopWatchService.kt
@@ -3,15 +3,34 @@ package com.naveen.stopwatchdemo
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import java.util.*
 
 class StopWatchService : Service() {
     override fun onBind(p0: Intent?): IBinder? = null
+    // instance of Timer class
+    private val timer = Timer()
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
+        timer.cancel()
         super.onDestroy()
+    }
+    // companion object
+    companion object{
+        const val CURRENT_TIME = "current time"
+        const val UPDATED_TIME = "updated time"
+    }
+
+    private inner class StopWatchTimerTask(private var time:Double):TimerTask(){
+        override fun run() {
+            val intent = Intent(UPDATED_TIME)
+            time++ // increase time by +1
+            intent.putExtra(UPDATED_TIME,time) // save current time to the intent
+            sendBroadcast(intent)
+        }
+
     }
 }
