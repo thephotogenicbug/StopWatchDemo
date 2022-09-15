bd29024c0df274ad79dac1329c7b017a875b6eb7
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-15 18:47:56 +0530

    Create Button Function using ViewBinding

--- app/src/main/java/com/naveen/stopwatchdemo/MainActivity.kt
+++ app/src/main/java/com/naveen/stopwatchdemo/MainActivity.kt
@@ -6,9 +6,38 @@ import com.naveen.stopwatchdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var isStarted = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            startOrStop()
+        }
+        binding.btnReset.setOnClickListener {
+            reset()
+        }
+    }
+
+    private fun startOrStop(){
+        if(isStarted){
+            stop()
+        } else{
+            start()
+        }
+    }
+
+    private fun start(){
+        binding.btnStart.text = "Stop"
+        isStarted = true
+    }
+    private fun stop(){
+        binding.btnStart.text = "Start"
+        isStarted = false
+
+    }
+    private fun reset(){
+
     }
 }
