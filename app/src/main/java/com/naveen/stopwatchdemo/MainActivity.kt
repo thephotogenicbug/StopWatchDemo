79ecb5b7ee73cd032d4b7c3697b014e2c50f2482
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-15 19:05:00 +0530

    Create ServiceIntent

--- app/src/main/java/com/naveen/stopwatchdemo/MainActivity.kt
+++ app/src/main/java/com/naveen/stopwatchdemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.naveen.stopwatchdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.naveen.stopwatchdemo.databinding.ActivityMainBinding
@@ -7,6 +8,7 @@ import com.naveen.stopwatchdemo.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var isStarted = false
+    private lateinit var serviceIntent : Intent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         binding.btnReset.setOnClickListener {
             reset()
         }
+        serviceIntent = Intent(applicationContext,StopWatchService::class.java)
     }
 
     private fun startOrStop(){
